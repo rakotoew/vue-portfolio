c3baa5eac4e462a03f45cca2b85de473da6de19f
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-10-22 20:28:41 +0200

    init and edit gitignore

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>A propos de moi :</h1>
+    <h3></h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Ewan Rakotoanosy</h1>
+      <h3>Développeur Full Stack</h3>
+      <h4>Etudiant en Master à l'Université Savoie Mont Blanc</h4>
+    </div>
+  </main>
+</template>
