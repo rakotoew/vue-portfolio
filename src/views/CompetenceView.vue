a448ee9df266360d55471b86b30e00d04d489332
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2022-10-23 00:20:06 +0200

    Adding competence page

---
+++ src/views/CompetenceView.vue
@@ -0,0 +1,79 @@
+<script></script>
+<template>
+  <div>
+    <h1>Mes Compétences Techniques :</h1>
+    <div>
+      <h1>Language de programation</h1>
+      <div>
+        <img
+          title="Python"
+          alt="Python logo"
+          src="@/assets/logo/logo-python.png"
+        />
+        <img
+          title="JavaScript"
+          alt="Javascript logo"
+          src="@/assets/logo/logo-javascript.png"
+        />
+        <img
+          title="TypeScript"
+          alt="TypeScript logo"
+          src="@/assets/logo/logo-typescript.png"
+        />
+        <img title="Java" alt="Java logo" src="@/assets/logo/logo-java.png" />
+        <img title="C" alt="C logo" src="@/assets/logo/logo-c.png" />
+        <img title="C++" alt="C++ logo" src="@/assets/logo/logo-c++.svg" />
+        <img
+          title="Ocaml"
+          alt="OCaml logo"
+          src="@/assets/logo/logo-ocaml.png"
+        />
+      </div>
+      <h1>Framework</h1>
+      <div>
+        <img
+          title="Node.js"
+          alt="node logo"
+          src="@/assets/logo/logo-node.png"
+        />
+        <img
+          title="React"
+          alt="React logo"
+          src="@/assets/logo/logo-react.svg"
+        />
+        <img title="Vue.js" alt="Vue logo" src="@/assets/logo/logo-vue.svg" />
+        <img
+          title="Angular"
+          alt="Angular logo"
+          src="@/assets/logo/logo-angular.png"
+        />
+      </div>
+      <h1>Base de Données</h1>
+      <div>
+        <img
+          title="MongoDB"
+          alt="MongoDB logo"
+          src="@/assets/logo/logo-mongodb.png"
+        />
+        <img title="SQL" alt="SQL logo" src="@/assets/logo/logo-sql.png" />
+        <img
+          title="Neo4J"
+          alt="Neo4J logo"
+          src="@/assets/logo/logo-neo4j.png"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  margin: 10px 10px 10px;
+  width: 125px;
+  height: 125px;
+  border-radius: 10px;
+}
+img:hover {
+  border-color: #2c3e50;
+}
+</style>
